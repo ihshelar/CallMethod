5cd60d0fb13d87c471ff7716d27f6a7ac2522d9c
Hemant <ihshelar@gmail.com>
2020-04-21 23:38:23 +0530

    test2

---
+++ src/package1/Method - Copy.java
@@ -0,0 +1,25 @@
+package package1;
+
+public class Method {
+
+	public String validate()
+	{
+	System.out.println("Let's start Call by method");
+	System.out.println("Let's start Call by method");
+	
+	System.out.println("Let's start Call by Develop");
+
+	
+	
+	return "PASS";
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
